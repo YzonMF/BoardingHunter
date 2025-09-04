18794b4c39c9206aa31b68e242f35857bd2d6184
Crizon <yzgit33@gmail.com>
2025-09-04 15:13:11 +0800

    gawin nio na senyu tapos na ang users migration

---
+++ app/Http/Controllers/RoomOwnerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\room_owner;
+use App\Http\Requests\Storeroom_ownerRequest;
+use App\Http\Requests\Updateroom_ownerRequest;
+
+class RoomOwnerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Storeroom_ownerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(room_owner $room_owner)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(room_owner $room_owner)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Updateroom_ownerRequest $request, room_owner $room_owner)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(room_owner $room_owner)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/RoomSeekerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\room_seeker;
+use App\Http\Requests\Storeroom_seekerRequest;
+use App\Http\Requests\Updateroom_seekerRequest;
+
+class RoomSeekerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Storeroom_seekerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(room_seeker $room_seeker)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(room_seeker $room_seeker)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Updateroom_seekerRequest $request, room_seeker $room_seeker)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(room_seeker $room_seeker)
+    {
+        //
+    }
+}
