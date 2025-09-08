c8baf82e0e66a8250d2317872d6875777f7c1072
HECATE-86 <marcosrussel13th@gmail.com>
2025-09-08 18:57:28 +0800

    labadeeh

---
+++ app/Http/Controllers/CreateReviewController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Create_Review;
+use App\Http\Requests\StoreCreate_ReviewRequest;
+use App\Http\Requests\UpdateCreate_ReviewRequest;
+
+class CreateReviewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCreate_ReviewRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Create_Review $create_Review)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Create_Review $create_Review)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCreate_ReviewRequest $request, Create_Review $create_Review)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Create_Review $create_Review)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_09_05_082046_create_create__reviews_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id('ReviewID'); 
+            $table->foreignId('SeekerID')
+                  ->constrained('seekers', 'SeekerID')
+                  ->onDelete('cascade'); 
+            $table->foreignId('AccommodationID')
+                  ->constrained('accommodations', 'AccommodationID')
+                  ->onDelete('cascade'); 
+            $table->unsignedTinyInteger('Rating'); 
+            $table->text('Comment')->nullable();
+            $table->timestamp('ReviewDate')->useCurrent();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
