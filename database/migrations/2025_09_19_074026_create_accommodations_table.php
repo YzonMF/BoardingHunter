3c5d8dc20523e7076ece255f989f904ecf6f49e3
unknown <wilsonhotdog21@gmail.com>
2025-09-19 16:34:56 +0800

    natatae ako

---
+++ database/migrations/2025_09_19_074026_create_accommodations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         Schema::create('accommodation', function (Blueprint $table) {
+            $table->bigIncrement('AccommodationID');
+            $table->unsignedBigInteger('OwnerID');
+            $table->string('Name', 100);
+            $table->enum('Type',['Boarding']);
+            $table->string('Description');
+            $table->string('Location');
+            $table->decimal('PricePerNight', 10, 2);
+            $table->decimal('PricePerMonth', 10, 2);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accommodations');
+    }
+};
