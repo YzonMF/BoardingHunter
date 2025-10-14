b5b1587142ce1c0f7cebcc11ae9bae78e594f046
Sairene Omipet <tootrene@gmail.com>
2025-10-14 09:47:22 +0800

    Initial commit

---
+++ database/migrations/2025_10_13_102015_create_photos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->id(photoID);
+            $table->int(AccommodationID);
+            $table->varchar('FilePathURL', 255);
+            $table->varchar('Caption',255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+}
