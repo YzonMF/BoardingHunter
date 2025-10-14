9e3a422ac81c697618ec03c005e42e716cd8f8ca
Yoame <bkznbev@gmail.com>
2025-10-14 15:16:28 +0000

    message

---
+++ app/Http/Controllers/SeekerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Seeker;
+use App\Http\Requests\StoreSeekerRequest;
+use App\Http\Requests\UpdateSeekerRequest;
+
+class SeekerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSeekerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Seeker $seeker)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Seeker $seeker)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSeekerRequest $request, Seeker $seeker)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Seeker $seeker)
+    {
+        //
+    }
+}
