64f1a400d299242858a1201f1b0292f19dbf38ae
wilson5th <143492411+wilson5th@users.noreply.github.com>
2025-10-27 19:49:50 +0800

    controller

--- app/Http/Controllers/AccommodationController.php
+++ app/Http/Controllers/AccommodationController.php
@@ -9,7 +9,12 @@ class AccommodationController extends Controller
     public function index()
     {
         // Eager load photos to avoid N+1 queries
-        $accommodations = Accommodation::with('photos', 'owner')->get();
+        $accommodations = Accommodation::with('photo', 'owner')->get();
         return view('accommodations.index', compact('accommodations'));
+    }
+    public function show(Accommodation $accommodations)
+    {
+        $accommodations = Accommodation::with('photo', 'owner')->get();
+        return view('accomodations.index', compact('accommodations'));
     }
 }
